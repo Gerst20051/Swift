7a7790d44ae08c4fa8ba7042cbfe9a0f21aed794
Andrew Gerst <gerst20051@gmail.com>
2014-08-20 01:51:41 -0400

    finished hello world app

--- HelloWorldApp/HelloWorldApp/ViewController.swift
+++ HelloWorldApp/HelloWorldApp/ViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var nameLabel: UILabel! = nil
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +22,9 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func helloWorldAction(textFieldValue: UITextField) {
+        nameLabel.text = "Hi \(textFieldValue.text)"
+    }
 
 }
 
