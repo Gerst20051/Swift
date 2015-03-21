75045f414d100304a0f2bdd4988e9ca1cf3f5b01
Andrew Gerst <gerst20051@gmail.com>
2015-03-20 22:42:55 -0400

    initial commit of birthdays app

---
+++ birthdays/birthdays/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  birthdays
+//
+//  Created by Andrew Gerst on 3/20/15.
+//  Copyright (c) 2015 Andrew Gerst. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
