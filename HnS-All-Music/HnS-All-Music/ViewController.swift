acbca0feffb47eaaa39fcc635efa482a209f8d0e
Andrew Gerst <gerst20051@gmail.com>
2016-06-23 16:04:54 -0400

    showing initial view controller

--- HnS-All-Music/HnS-All-Music/ViewController.swift
+++ HnS-All-Music/HnS-All-Music/ViewController.swift
@@ -1,25 +1,10 @@
-//
-//  ViewController.swift
-//  HnS-All-Music
-//
-//  Created by Andrew Gerst on 6/23/16.
-//  Copyright © 2016 Andrew Gerst. All rights reserved.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        self.view.backgroundColor = UIColor.redColor()
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
 
 }
-
