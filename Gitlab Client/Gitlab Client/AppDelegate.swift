7c059932f75956541b79d78706351daa263de725
Andrew Gerst <gerst20051@gmail.com>
2016-07-27 21:21:15 -0400

    initial commit of gitlab client mac os app

---
+++ Gitlab Client/Gitlab Client/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  Gitlab Client
+//
+//  Created by Andrew Gerst on 7/27/16.
+//  Copyright © 2016 Andrew Gerst. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
