923abb570d8547142984c5b8576b5985a296546b
Andrew Gerst <gerst20051@gmail.com>
2016-07-27 21:32:30 -0400

    basic initial cleanup

--- Gitlab Client/Gitlab Client/AppDelegate.swift
+++ Gitlab Client/Gitlab Client/AppDelegate.swift
@@ -1,11 +1,3 @@
-//
-//  AppDelegate.swift
-//  Gitlab Client
-//
-//  Created by Andrew Gerst on 7/27/16.
-//  Copyright © 2016 Andrew Gerst. All rights reserved.
-//
-
 import Cocoa
 
 @NSApplicationMain
@@ -13,15 +5,8 @@ class AppDelegate: NSObject, NSApplicationDelegate {
 
     @IBOutlet weak var window: NSWindow!
 
+    func applicationDidFinishLaunching(_ aNotification: Notification) {}
 
-    func applicationDidFinishLaunching(_ aNotification: Notification) {
-        // Insert code here to initialize your application
-    }
-
-    func applicationWillTerminate(_ aNotification: Notification) {
-        // Insert code here to tear down your application
-    }
-
+    func applicationWillTerminate(_ aNotification: Notification) {}
 
 }
-
