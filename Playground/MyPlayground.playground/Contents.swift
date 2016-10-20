18ac15ba0def242a60541daaf65970d9b459174e
Andrew Gerst <gerst20051@gmail.com>
2016-10-19 22:18:00 -0400

    update playground to swift 3

--- Playground/MyPlayground.playground/Contents.swift
+++ Playground/MyPlayground.playground/Contents.swift
@@ -4,7 +4,7 @@ import UIKit
 
 var mainWidth: CGFloat = 320.0
 var columnWidth: CGFloat = mainWidth / 3.0
-var remainder = columnWidth % floor(columnWidth)
+var remainder = columnWidth.truncatingRemainder(dividingBy: floor(columnWidth))
 
 
 var test = "en_US,fr_CA,es_US"
@@ -12,4 +12,4 @@ var test = "en_US,fr_CA,es_US"
 var arr = test.characters.split { $0 == "," }.map { String($0) }.map { $0.characters.split { $0 == "_" }.map { String($0) } }.map { $0.first! }
 arr
 
-NSUUID().UUIDString
+NSUUID().uuidString
