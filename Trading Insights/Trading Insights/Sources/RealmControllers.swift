f180f5352bf299ca92adb781ae3d400d0313b37e
Andrew Gerst <gerst20051@gmail.com>
2016-12-10 18:09:59 -0500

    new project boilerplate updates

---
+++ Trading Insights/Trading Insights/Sources/RealmControllers.swift
@@ -0,0 +1,5 @@
+import RealmSwift
+
+class ModelController {
+
+}
--- Trading Insights/Trading InsightsTests/Trading_InsightsTests.swift
+++ Trading Insights/Trading InsightsTests/Trading_InsightsTests.swift
@@ -1,36 +1,21 @@
-//
-//  Trading_InsightsTests.swift
-//  Trading InsightsTests
-//
-//  Created by Andrew Gerst on 12/10/16.
-//  Copyright © 2016 Andrew Gerst. All rights reserved.
-//
-
 import XCTest
+
 @testable import Trading_Insights
 
 class Trading_InsightsTests: XCTestCase {
-    
+
     override func setUp() {
         super.setUp()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
     }
-    
+
     override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
         super.tearDown()
     }
-    
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-    
+
+    func testExample() {}
+
     func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
+        self.measure {}
     }
-    
+
 }
