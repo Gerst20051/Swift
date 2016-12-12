f4499e9bac30e3784a5c676fc24bdc9f83640205
Andrew Gerst <gerst20051@gmail.com>
2016-12-12 00:38:54 -0500

    add remaining text fields for adding new trade ideas

--- Trading Insights/Trading Insights/Sources/ViewControllers/AddIdeaViewController.swift
+++ Trading Insights/Trading Insights/Sources/ViewControllers/AddIdeaViewController.swift
@@ -10,6 +10,9 @@ class AddIdeaViewController: BaseViewController {
     fileprivate let tickerField = UITextField()
     fileprivate let tickerNameField = UITextField()
     fileprivate let ideaSourceField = UITextField()
+    fileprivate let ideaSentimentField = UITextField()
+    fileprivate let tradedField = UITextField()
+    fileprivate let holdingDurationField = UITextField()
     fileprivate let tagsField = WSTagsField()
 
     override func viewDidLoad() {
@@ -19,12 +22,14 @@ class AddIdeaViewController: BaseViewController {
 
     func setupInterface() {
         view.backgroundColor = .white
-        // view.backgroundColor = .red
         createToolbar()
         createScrollView()
         createTickerField()
         createTickerNameField()
-        // createIdeaSourceField()
+        createIdeaSourceField()
+        createIdeaSentimentField()
+        createTradedField()
+        createHoldingDurationField()
         createTagsField()
         createTagsFieldHandlers()
         addInterfaceContraints()
@@ -84,7 +89,7 @@ extension AddIdeaViewController {
 
 }
 
-extension AddIdeaViewController {
+extension AddIdeaViewController: UITextFieldDelegate {
 
     func addFieldContraints() {
         let margin: CGFloat = 12.0
@@ -95,32 +100,82 @@ extension AddIdeaViewController {
             make.width.equalTo(scrollView).offset(-2.0 * margin)
         }
         tickerNameField.snp.makeConstraints { make -> Void in
-            make.bottom.equalTo(tagsField.snp.top).offset(-margin)
+            make.bottom.equalTo(ideaSourceField.snp.top).offset(-2.0 * margin)
             make.left.equalTo(scrollView).offset(margin)
             make.top.equalTo(tickerField.snp.bottom).offset(2.0 * margin)
             make.width.equalTo(scrollView).offset(-2.0 * margin)
         }
+        ideaSourceField.snp.makeConstraints { make -> Void in
+            make.bottom.equalTo(ideaSentimentField.snp.top).offset(-2.0 * margin)
+            make.left.equalTo(scrollView).offset(margin)
+            make.top.equalTo(tickerNameField.snp.bottom).offset(2.0 * margin)
+            make.width.equalTo(scrollView).offset(-2.0 * margin)
+        }
+        ideaSentimentField.snp.makeConstraints { make -> Void in
+            make.bottom.equalTo(tradedField.snp.top).offset(-2.0 * margin)
+            make.left.equalTo(scrollView).offset(margin)
+            make.top.equalTo(ideaSourceField.snp.bottom).offset(2.0 * margin)
+            make.width.equalTo(scrollView).offset(-2.0 * margin)
+        }
+        tradedField.snp.makeConstraints { make -> Void in
+            make.bottom.equalTo(holdingDurationField.snp.top).offset(-2.0 * margin)
+            make.left.equalTo(scrollView).offset(margin)
+            make.top.equalTo(ideaSentimentField.snp.bottom).offset(2.0 * margin)
+            make.width.equalTo(scrollView).offset(-2.0 * margin)
+        }
+        holdingDurationField.snp.makeConstraints { make -> Void in
+            make.bottom.equalTo(tagsField.snp.top).offset(-margin) // tagsField already has a margin
+            make.left.equalTo(scrollView).offset(margin)
+            make.top.equalTo(tradedField.snp.bottom).offset(2.0 * margin)
+            make.width.equalTo(scrollView).offset(-2.0 * margin)
+        }
         tagsField.snp.makeConstraints { make -> Void in
             make.height.equalTo(44.0)
-            make.top.equalTo(tickerNameField.snp.bottom).offset(margin)
+            make.top.equalTo(holdingDurationField.snp.bottom).offset(margin)
             make.width.equalTo(scrollView)
         }
     }
 
     func createTickerField() {
+        tickerField.delegate = self
         tickerField.placeholder = "Ticker"
         tickerField.tintColor = AppColor.tag
         scrollView.addSubview(tickerField)
     }
 
     func createTickerNameField() {
+        tickerNameField.delegate = self
         tickerNameField.placeholder = "Name"
         tickerNameField.tintColor = AppColor.tag
         scrollView.addSubview(tickerNameField)
     }
 
     func createIdeaSourceField() {
+        ideaSourceField.delegate = self
+        ideaSourceField.placeholder = "Source"
+        ideaSourceField.tintColor = AppColor.tag
         scrollView.addSubview(ideaSourceField)
+    }
+
+    func createIdeaSentimentField() {
+        ideaSentimentField.delegate = self
+        ideaSentimentField.placeholder = "Sentiment"
+        ideaSentimentField.tintColor = AppColor.tag
+        scrollView.addSubview(ideaSentimentField)
+    }
+
+    func createTradedField() {
+        tradedField.delegate = self
+        tradedField.placeholder = "Traded"
+        tradedField.tintColor = AppColor.tag
+        scrollView.addSubview(tradedField)
+    }
+
+    func createHoldingDurationField() {
+        holdingDurationField.delegate = self
+        holdingDurationField.placeholder = "Holding Duration"
+        holdingDurationField.tintColor = AppColor.tag
+        scrollView.addSubview(holdingDurationField)
     }
 
     func createTagsField() {
@@ -128,21 +183,40 @@ extension AddIdeaViewController {
         tagsField.placeholder = AppString.EnterIdeaIndicators
         tagsField.tintColor = AppColor.tag
         scrollView.addSubview(tagsField)
+        // need to scroll up scrollview when keyboard is open
     }
 
     func createTagsFieldHandlers() {
         tagsField.onDidAddTag = { _ in // update local list
             print("DidAddTag => \(self.tagsField.tags)")
         }
-
         tagsField.onDidRemoveTag = { _ in // update local list
             print("DidRemoveTag => \(self.tagsField.tags)")
         }
-
+        tagsField.onDidChangeText = { _, text in // use this to update an autocomplete list?
+            print("DidChangeText => \(text)")
+        }
+        tagsField.onShouldReturn = { tagsField -> Bool in
+            tagsField.endEditing()
+            return false
+        }
         tagsField.onVerifyTag = { _, text in
             print("VerifyTag => \(text)")
             return true
         }
+    }
+
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        if textField.isEqual(tickerField) {
+            tickerField.text = ((textField.text ?? "") as NSString).replacingCharacters(in: range, with: string.uppercased())
+            return false
+        }
+        return true
+    }
+
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
     }
 
 }
