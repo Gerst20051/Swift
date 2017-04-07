87c9ff5d037d0c387f376123ae82f16329abe336
Andrew Gerst <gerst20051@gmail.com>
2017-04-06 23:49:36 -0400

    app boilerplate project

---
+++ AppBoilerplate/AppBoilerplateTests/AppBoilerplateTests.swift
@@ -0,0 +1,21 @@
+import XCTest
+
+@testable import AppBoilerplate
+
+class AppBoilerplateTests: XCTestCase {
+
+    override func setUp() {
+        super.setUp()
+    }
+
+    override func tearDown() {
+        super.tearDown()
+    }
+
+    func testExample() {}
+
+    func testPerformanceExample() {
+        self.measure {}
+    }
+
+}
