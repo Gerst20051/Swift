e3be43455305ae946ec1610f3418fa060048a03a
Andrew Gerst <gerst20051@gmail.com>
2017-04-06 22:10:46 -0400

    initial project creation cleanup

--- PodBuddy/PodBuddy/ViewController.swift
+++ PodBuddy/PodBuddy/ViewController.swift
@@ -1,25 +1,13 @@
-//
-//  ViewController.swift
-//  PodBuddy
-//
-//  Created by Andrew Gerst on 4/6/17.
-//  Copyright © 2017 Andrew Gerst. All rights reserved.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
 
-
 }
-
