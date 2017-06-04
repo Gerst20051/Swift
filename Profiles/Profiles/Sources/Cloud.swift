632d7b195c15a19d6669a1b6a2139f361e3e9bdd
Andrew Gerst <gerst20051@gmail.com>
2017-06-04 15:55:48 -0400

    inital profiles commit

---
+++ Profiles/Profiles/Sources/Cloud.swift
@@ -0,0 +1,43 @@
+import Alamofire
+import AlamofireObjectMapper
+import PromiseKit
+
+class CloudUtils {
+
+    static let basedomain = "hnswave.co"
+    static let baseurl = "http://\(basedomain):8003"
+
+}
+
+class Networking {
+
+    static let manager: SessionManager = {
+        let serverTrustPolicies: [String: ServerTrustPolicy] = [
+            CloudUtils.basedomain: .disableEvaluation
+        ]
+        let configuration = URLSessionConfiguration.default
+        configuration.httpAdditionalHeaders = SessionManager.defaultHTTPHeaders
+        return SessionManager(configuration: configuration, serverTrustPolicyManager: ServerTrustPolicyManager(policies: serverTrustPolicies))
+    }()
+
+}
+
+class Cloud {
+
+    class func searchPodcasts(keywords: String) -> Promise<[PodcastSearchResult]> {
+        return Promise { fulfill, reject in
+            _ = Networking.manager.request("\(CloudUtils.baseurl)/search/\(keywords)", method: .get).validate().responseArray { (response: DataResponse<[PodcastSearchResult]>) in
+                if response.result.isSuccess {
+                    if let results = response.result.value {
+                        fulfill(results)
+                    } else {
+                        reject(PromiseError.invalidResults())
+                    }
+                } else {
+                    reject(PromiseError.apiFailure(response.result.error))
+                }
+            }
+        }
+    }
+
+}
